9894e7945e0c8443f5b87b0bb70b62afad124221
alex <YOUR>
2020-07-23 22:25:58 -0300

    Commit inicial

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+
+<html lang="pt-br">
+<hea>
+    <meta charset="UTF-8">
+    <title></title>
+    <meta name="author" content="">
+    <meta name="description" content="">
+    <meta name="viewport" content="width-device-width, initial-scale-1">
+</hea>
+
+<body>
+<p>FORMULÁRIO PARA INSCRIÇÃO DE COMPETIDORES</p>
+<form action="script.php" method="post">
+    <p>Seu nome: <input type="text" name="nome"></p>
+    <p>Sua idade: <input type="text" name="idade"></p>
+    <p><input type="submit" value="Enviar dados do competidor"></p>
+</form>
+</body>
+</html>
